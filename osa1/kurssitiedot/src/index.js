ff64da75dc69ba988a6e544a5ea524f2ea93ae43
Unknown <valtteri.nietula@gmail.com>
2019-04-12 13:13:52 +0300

    1.4

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = {
+    const parts = [{
         name: 'Reactin perusteet',
         exercises: 10
-    }
-    const part2 = {
+    },
+    {
         name: 'Tiedonvälitys propseilla',
         exercises: 7
-    }
-    const part3 = {
+    },
+    {
         name: 'Komponenttien tila',
         exercises: 14
-    }
+    }]
     
     
     
@@ -37,24 +37,24 @@ const App = () => {
     )
     const Content = (props) => (
         <>
-            <Part data={props.part1}  />
-            <Part data={props.part2} />
-            <Part data={props.part3}  />
+            <Part data={props.osat[0]}  />
+            <Part data={props.osat[1]} />
+            <Part data={props.osat[2]}  />
         </>
 
     )
 
     const Total = (props) => (
         <>
-            <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+            <p>yhteensä {props.osat[0].exercises + props.osat[1].exercises + props.osat[2].exercises} tehtävää</p>
         </>
     )
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3}  />
-            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+            <Content osat={parts}  />
+            <Total osat = {parts} />
         </div>
     )
 }
